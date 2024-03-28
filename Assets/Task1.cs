e1e6cbae366eb8a454f3bb16d9499dfc5421c6c6
Dylan Torres <glaciershield@gmail.com>
2024-03-28 14:32:04 -0400

    Task 1 Setup
    Slight setup to Task 1.

--- Assets/Task1.cs
+++ Assets/Task1.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class Task1 : MonoBehaviour
 {
+    string[] playerFirstNames = new string[19];
+    string[] playerLastNames = new string[25];
+
+    Queue<string> initialLogin;
+
     // Start is called before the first frame update
     void Start()
     {
